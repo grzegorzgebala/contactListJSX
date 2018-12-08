2c92b207f20e9e6cfcc24a324e6933a3b95eaafd
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-12-08 21:46:36 +0100

    trial commit

--- components/App.js
+++ components/App.js
@@ -1,10 +1,13 @@
 var App = React.createClass({
   render: function() {
     return (
+    /*
+      <div className={'app'}>
+      	<ContactForm {contact: contactForm}>
+      	<Contacts {items: contacts}, {}>*/
       React.createElement('div', {className: 'app'},
         React.createElement(ContactForm, {contact: contactForm}),
         React.createElement(Contacts, {items: contacts}, {})
-      )
-    );
-  }
+    	)
+    )}
 });
--- components/Contact.js
+++ components/Contact.js
@@ -5,17 +5,27 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem, box'},
-        React.createElement('img', {
-          className: 'contactImage, image',
-          src: 'http://www.stickpng.com/assets/images/585e4beacb11b227491c3399.png'
-        }),
-        React.createElement('p', {className: 'contactLabel, name'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel, name'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail, name', href: 'mailto:' + this.props.item.email},
-          this.props.item.email
-        )
-      )
+      <div className={'contactItem, box'}>
+        <img className={'contactImage, image'} src={'http://www.stickpng.com/assets/images/585e4beacb11b227491c3399.png'}/>
+        <p className={'contactLabel, name'}>
+          Imię: {this.props.contact.firstName}
+        </p>
+        
+        <a href={'mailto:' + this.props.item.email}>
+            {this.props.item.email}
+        </a>
+     
+     // React.createElement('div', {className: 'contactItem, box'},
+       // React.createElement('img', {
+       //   className: 'contactImage, image',
+         // src: 'http://www.stickpng.com/assets/images/585e4beacb11b227491c3399.png'
+     //   }),
+      //  React.createElement('p', {className: 'contactLabel, name'}, 'Imię: ' + this.props.item.firstName),
+       // React.createElement('p', {className: 'contactLabel, name'}, 'Nazwisko: ' + this.props.item.lastName),
+       // React.createElement('a', {className: 'contactEmail, name', href: 'mailto:' + this.props.item.email},
+        //  this.props.item.email
+       // )
+    //  )*/
     )
   },
 });
